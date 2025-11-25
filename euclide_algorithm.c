eb28e993ebf775e743c81b78feb45f66b91dbf60
Nouyom Chekem <chekeric@outlook.com>
2025-11-25 15:34:07 -0500

    add euclide algorithme

---
+++ euclide_algorithm.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int pgcd(int m, int n) {
+  if (m < n) {
+    return pgcd(n, m);
+  }
+  int remainder = m % n;
+  if (remainder == 0) {
+    return n;
+  } else {
+    return pgcd(n, remainder);
+  }
+}
+
+int main(void) {
+
+  int n, m, result;
+
+  printf("Euclid's algorithm\n");
+  printf(" Given two positive integers m and n, find their greatest common divisor, that is, the largest positive integer that evenly divides both m and n.\n");
+  printf("Enter n value: ");
+  scanf("%i", &n);
+  if (n < 0) {
+    printf("value of n should be a positive number\n");
+    return -1;
+  }
+
+  printf("Enter m value: ");
+  scanf("%i", &m);
+  if (m < 0) {
+    printf("value of m should be a positive number\n\n");
+    return -1;
+  }
+
+  result =  pgcd(m, n);
+
+  printf("The greatest common divisor of m and n is : %i\n", result);
+
+  return 0;
+}
